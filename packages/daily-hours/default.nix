cb00d3a19c9981cdbade67bfb6bd2779c44bf209
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-03-02 08:49:37 +0100

    packages(daily-hours): Rich view

--- packages/daily-hours/default.nix
+++ packages/daily-hours/default.nix
@@ -2,10 +2,12 @@
   python3,
   writeShellApplication,
 }:
-
+let
+  pythonSet = python3.withPackages (ps: [ ps.rich ]);
+in
 writeShellApplication {
   name = "daily-hours";
-  runtimeInputs = [ python3 ];
+  runtimeInputs = [ pythonSet ];
   text = ''
     exec python3 ${./uptime.py} "$@"
   '';
