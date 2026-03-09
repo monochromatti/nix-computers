d63cee4f94fe3e204d0e412f615bd8fdec876473
Mattias Matthiesen <mattmatthi@hotmail.com>
2026-03-09 16:49:02 +0100

    home: Shell functions for agent CLIs

--- dotfiles/init.zsh
+++ dotfiles/init.zsh
@@ -6,3 +6,18 @@ function y() {
     fi
     rm -f -- "$tmp"
 }
+
+pi() {
+  local npm_prefix="$HOME/.local/share/pi/npm"
+  local npm_cache="$HOME/.cache/pi/npm"
+  mkdir -p "$npm_prefix" "$npm_cache"
+  eval "$(nix run github:fornybar/agents#aienv -- --azure)"
+  export npm_config_prefix="$npm_prefix"
+  export npm_config_cache="$npm_cache"
+  export AZURE_OPENAI_API_KEY="$AZURE_API_KEY"
+  export AZURE_OPENAI_RESOURCE_NAME="$AZURE_RESOURCE_NAME"
+  export PATH="$npm_prefix/bin:$PATH"
+  nix run github:numtide/llm-agents.nix#pi -- "$@"
+}
+
+amp() { nix run github:numtide/llm-agents.nix#amp -- "$@"; }
--- modules/home-manager/aliases.nix
+++ modules/home-manager/aliases.nix
@@ -4,7 +4,6 @@
     home.shellAliases = {
       lg = "lazygit";
       zed = "zeditor .";
-      agentenv = "nix shell github:numtide/llm-agents.nix#amp github:numtide/llm-agents.nix#tuicr github:fornybar/pydeptree";
     };
   };
 }
