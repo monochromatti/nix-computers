6502675baa1ad5e26ba784352899e639dc3df4d1
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-04-20 07:48:15 +0200

    shell: Move from p10k to starhip

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -16,6 +16,47 @@
         starship = {
           enable = true;
           enableZshIntegration = true;
+          settings = {
+            add_newline = true;
+            format = "$os$directory$git_branch$git_status\n$character";
+            right_format = "$status$cmd_duration$jobs$direnv$python$nodejs$golang$rust$java$kubernetes$terraform$aws$gcloud$nix_shell$time";
+
+            os.disabled = false;
+
+            directory = {
+              truncation_length = 3;
+              truncate_to_repo = false;
+              read_only = " ";
+            };
+
+            git_branch.symbol = " ";
+
+            cmd_duration = {
+              min_time = 3000;
+              show_milliseconds = false;
+            };
+
+            jobs.symbol = " ";
+
+            direnv.disabled = false;
+            status.disabled = false;
+            kubernetes.disabled = false;
+            terraform.disabled = false;
+            aws.disabled = false;
+            gcloud.disabled = false;
+
+            nix_shell = {
+              symbol = " ";
+              impure_msg = "impure";
+              pure_msg = "pure";
+              unknown_msg = "";
+            };
+
+            time = {
+              disabled = false;
+              time_format = "%T";
+            };
+          };
         };
 
         zoxide = {
@@ -38,26 +79,6 @@
           autocd = true;
           autosuggestion.enable = true;
           syntaxHighlighting.enable = true;
-          plugins = [
-            {
-              name = "powerlevel10k-config";
-              src = pkgs.lib.cleanSource ../../dotfiles;
-              file = "p10k.zsh";
-            }
-          ];
-          zplug = {
-            enable = true;
-            plugins = [
-              { name = "zsh-users/zsh-autosuggestions"; }
-              {
-                name = "romkatv/powerlevel10k";
-                tags = [
-                  "as:theme"
-                  "depth:1"
-                ];
-              }
-            ];
-          };
           envExtra = ''
             zstyle ':autocomplete:*' list-lines 5
           '';
