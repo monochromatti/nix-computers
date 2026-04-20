86b98802954b1d45cf820bb5859bc5d9760006ae
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-04-20 09:19:18 +0200

    shell: Pure starship preset

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -17,44 +17,48 @@
           enable = true;
           enableZshIntegration = true;
           settings = {
-            add_newline = true;
-            format = "$os$directory$git_branch$git_status\n$character";
-            right_format = "$status$cmd_duration$jobs$direnv$python$nodejs$golang$rust$java$kubernetes$terraform$aws$gcloud$nix_shell$time";
+            format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
 
-            os.disabled = false;
+            directory.style = "blue";
 
-            directory = {
-              truncation_length = 3;
-              truncate_to_repo = false;
-              read_only = " ";
+            character = {
+              success_symbol = "[❯](purple)";
+              error_symbol = "[❯](red)";
+              vimcmd_symbol = "[❮](green)";
             };
 
-            git_branch.symbol = " ";
+            git_branch = {
+              format = "[$branch]($style)";
+              style = "bright-black";
+            };
+
+            git_status = {
+              format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+              style = "cyan";
+              conflicted = "​";
+              untracked = "​";
+              modified = "​";
+              staged = "​";
+              renamed = "​";
+              deleted = "​";
+              stashed = "≡";
+            };
+
+            git_state = {
+              format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
+              style = "bright-black";
+            };
 
             cmd_duration = {
-              min_time = 3000;
-              show_milliseconds = false;
+              format = "[$duration]($style) ";
+              style = "yellow";
             };
 
-            jobs.symbol = " ";
-
-            direnv.disabled = false;
-            status.disabled = false;
-            kubernetes.disabled = true;
-            terraform.disabled = false;
-            aws.disabled = false;
-            gcloud.disabled = false;
-
-            nix_shell = {
-              symbol = " ";
-              impure_msg = "impure";
-              pure_msg = "pure";
-              unknown_msg = "";
-            };
-
-            time = {
-              disabled = false;
-              time_format = "%T";
+            python = {
+              format = "[$virtualenv]($style) ";
+              style = "bright-black";
+              detect_extensions = [ ];
+              detect_files = [ ];
             };
           };
         };
