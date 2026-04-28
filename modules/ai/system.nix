10ff04e3cd3aa3ee86128169519d744fb1c31531
Mattias Matthiesen <mattmatthi@hotmail.com>
2026-04-28 21:36:07 +0200

    ai: Simplify module structure

---
+++ modules/ai/system.nix
@@ -0,0 +1,26 @@
+{ inputs, ... }:
+let
+  aiModule =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [
+        inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.ai
+      ];
+    };
+in
+{
+  perSystem =
+    { pkgs, config, ... }:
+    {
+      packages.ai = pkgs.buildEnv {
+        name = "ai";
+        paths = [
+          config.packages.pi
+          config.packages.pi-dev
+        ];
+      };
+    };
+
+  flake.modules.darwin.ai = aiModule;
+  flake.modules.nixos.ai = aiModule;
+}
