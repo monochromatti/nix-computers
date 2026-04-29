80e0d795ac6760d18abec2b7b1b11bbd19058a6e
Mattias Matthiesen <mattmatthi@hotmail.com>
2026-04-29 18:58:20 +0200

    ghostty: Liquid glass on darwin

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -8,6 +8,24 @@
     }:
     let
       opacity = inputs.self.desktop.opacity;
+
+      commonSettings = {
+        font-family = "JetBrains Mono";
+        window-padding-x = 8;
+        window-padding-y = 8;
+        cursor-style = "block";
+        background-opacity = opacity;
+        background-opacity-cells = true;
+      };
+
+      linuxSettings = {
+        font-size = 11;
+      };
+
+      darwinSettings = {
+        background-blur = "macos-glass-regular";
+        macos-titlebar-style = "transparent";
+      };
     in
     {
       xdg.enable = true;
@@ -15,17 +33,10 @@
       programs.ghostty = {
         enable = true;
         package = if pkgs.stdenv.isLinux then pkgs.ghostty else null;
-        settings = {
-          font-family = "JetBrains Mono";
-          window-padding-x = 8;
-          window-padding-y = 8;
-          cursor-style = "block";
-          background-opacity = opacity;
-          background-opacity-cells = true;
-        }
-        // lib.optionalAttrs pkgs.stdenv.isLinux {
-          font-size = 11;
-        };
+        settings =
+          commonSettings
+          // lib.optionalAttrs pkgs.stdenv.isLinux linuxSettings
+          // lib.optionalAttrs pkgs.stdenv.isDarwin darwinSettings;
       };
     };
 }
