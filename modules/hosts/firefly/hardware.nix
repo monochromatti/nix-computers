41a578ae2137aa565fe32573ff4c800ca4d21dcf
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-07-08 15:36:48 +0200

    flake: update

--- modules/hosts/firefly/hardware.nix
+++ modules/hosts/firefly/hardware.nix
@@ -4,7 +4,30 @@
     (
       { pkgs, lib, ... }:
       {
+        boot.initrd.availableKernelModules = [
+          "xhci_pci"
+          "thunderbolt"
+          "nvme"
+          "usb_storage"
+          "sd_mod"
+        ];
+        boot.kernelModules = [ "kvm-intel" ];
         boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_18;
+        boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+        fileSystems."/" = {
+          device = "/dev/disk/by-uuid/053849ba-b8f4-4350-9ebe-a36b49abe87c";
+          fsType = "ext4";
+        };
+
+        fileSystems."/boot/efi" = {
+          device = "/dev/disk/by-uuid/9834-9FCB";
+          fsType = "vfat";
+          options = [
+            "fmask=0022"
+            "dmask=0022"
+          ];
+        };
 
         swapDevices = [ { label = "swap"; } ];
 
