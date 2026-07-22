b3e6bd76b1ae60c788c9b08ecfa868c2b6896956
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-07-22 08:51:21 +0200

    firefly: generic start-vm command

--- modules/hosts/firefly/work.nix
+++ modules/hosts/firefly/work.nix
@@ -17,20 +17,37 @@
           ];
         };
 
-        environment.shellAliases = {
-          start-vidar-prod = ''
-            az vm start --subscription 584a2d66-5adc-45d5-b796-9d69d54154d6 --resource-group asgard --name vidar
-          '';
-          stop-vidar-prod = ''
-            az vm deallocate --subscription 584a2d66-5adc-45d5-b796-9d69d54154d6 --resource-group asgard --name vidar
-          '';
-          start-vidar-dev = ''
-            az vm start --subscription 5111c8c6-28f3-4b11-a07f-0aef3ed4721d --resource-group asgard --name vidar
-          '';
-          stop-vidar-dev = ''
-            az vm deallocate --subscription 5111c8c6-28f3-4b11-a07f-0aef3ed4721d --resource-group asgard --name vidar
-          '';
-        };
+        environment.systemPackages = [
+          (pkgs.writeShellApplication {
+            name = "start-vm";
+            runtimeInputs = [ pkgs.azure-cli ];
+            text = ''
+              if [ "$#" -ne 2 ]; then
+                echo "usage: start-vm <machine> <env>" >&2
+                exit 2
+              fi
+
+              machine="$1"
+              env="$2"
+
+              case "$env" in
+                prod)
+                  subscription="584a2d66-5adc-45d5-b796-9d69d54154d6"
+                  ;;
+                dev)
+                  subscription="5111c8c6-28f3-4b11-a07f-0aef3ed4721d"
+                  ;;
+                *)
+                  echo "unknown env: $env" >&2
+                  echo "expected: prod, dev" >&2
+                  exit 2
+                  ;;
+              esac
+
+              az vm start --subscription "$subscription" --resource-group asgard --name "$machine"
+            '';
+          })
+        ];
       }
     )
   ];
