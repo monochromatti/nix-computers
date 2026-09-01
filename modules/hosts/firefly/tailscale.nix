2b493639f07e5cc6665525257b42de16eea1824d
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-09-01 12:11:55 +0200

    fix(tailscale): preserve disabled DNS when switching tailnets

--- modules/hosts/firefly/tailscale.nix
+++ modules/hosts/firefly/tailscale.nix
@@ -12,10 +12,12 @@
       tailnets = {
         dev = {
           loginServer = "https://head.dev.fornybar.eviny.io";
+          acceptDns = false;
           sshUser = "odin";
         };
         prod = {
           loginServer = "https://head.fornybar.eviny.io";
+          acceptDns = false;
           sshUser = "odin";
         };
       };
