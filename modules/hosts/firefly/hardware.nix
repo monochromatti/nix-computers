e01ac20380b074cabe17faf66ccbd6f4d881d4cf
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-09-17 07:52:37 +0200

    fix(firefly): use the 7.2 kernel series
    Use the newer kernel line to address Wi-Fi driver resets.

--- modules/hosts/firefly/hardware.nix
+++ modules/hosts/firefly/hardware.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
   flake.modules.nixos."host/firefly/hardware" = (
-    { pkgs, lib, ... }:
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
     {
       boot.initrd.availableKernelModules = [
         "xhci_pci"
@@ -11,7 +16,7 @@
         "sd_mod"
       ];
       boot.kernelModules = [ "kvm-intel" ];
-      boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_18;
+      boot.kernelPackages = lib.mkForce pkgs.linuxPackages_7_2;
       boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
       fileSystems."/" = {
@@ -31,7 +36,14 @@
       swapDevices = [ { label = "swap"; } ];
 
       hardware = {
-        # nvidia.package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.production;
+        nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "595.91.07";
+          sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
+          sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
+          openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
+          settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
+          persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
+        };
         bluetooth = {
           enable = true;
           powerOnBoot = true;
